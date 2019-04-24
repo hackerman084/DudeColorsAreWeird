0305d22174ab0b03bc72810bc22e650eb5a70350
Astha Singhal <asingha3@umiacs.umd.edu>
2019-04-24 09:55:13 -0400

    got more of the response data

--- code/trial2.js
+++ code/trial2.js
@@ -1,6 +1,6 @@
 var counter = -1; 
 var imageNames = ["introverts", "commodities", "shopping", "burger", "gaming"];
-var paletteNames = ["calm", "disturbing", "exciting", "negative", "playful", "positive", "positive", "serious", "trustworthy"];
+var paletteNames = ["disturbing", "negative", "serious", "trustworthy", "calm", "playful", "positive", "exciting"];
 var imgName = "";
 var palette = "";
 
@@ -49,14 +49,33 @@ function onNextPress(){
 	initTrial();
 }
 
+function getRadio(nameVal){
+	return document.querySelector("input[name = \"" + nameVal + "\"]:checked").value;
+}
+
+function getText(id){
+	return document.getElementById(id).value;
+}
 function getInfo(){
-	var question1 = document.querySelector('input[name = "curr_state"]:checked').value;
+	var question1 = getRadio("curr_state");
 	console.log("QUESTION 1: " +question1);
 	var question2 = new Array(5);
 	for(var i = 0; i < 5; i++){
-		question2[i] = document.getElementById("emotion" + i).value;
+		question2[i] = getText("emotion" + i);
 	}
 	console.log("QUESTION 2: " +question2);
+
+	var question3 = paletteNames[getRadio("palette1")];
+	console.log("QUESTION 3: " +question3);
+
+	var question4 = getRadio("satisfied_1");
+	console.log("QUESTION 4: " +question4);
+
+	var question5 = paletteNames[getRadio("palette2")]
+	console.log("QUESTION 5: " +question5);
+
+	var question6 = getRadio("satisfied_2");
+	console.log("QUESTION 6: " +question6);
 
 }
 function onSubmitPress(){

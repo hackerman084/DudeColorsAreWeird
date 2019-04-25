6b6e7c1486ed7fb255d95d1ffa190cfdb9749891
Astha Singhal <asingha3@umiacs.umd.edu>
2019-04-25 14:48:09 -0400

    trying to get databse connected

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 
 var MongoClient = require('mongodb').MongoClient;
 var database;
+var collection; 
 
 var test = "mongodb://test:test123@ds147436.mlab.com:47436/heroku_9q5nxrrj"
 MongoClient.connect(test || "mongodb://localhost/responses",{ useNewUrlParser: true }, function (err,datab){
@@ -12,11 +13,10 @@ MongoClient.connect(test || "mongodb://localhost/responses",{ useNewUrlParser: t
 	//creating table
 	database = datab.db('heroku_9q5nxrrj')
 	console.log(process.env.MONGODB_URI);
-	var collection = database.collection('test');
+	collection = database.collection('test');
 	var docs = [{mykey:1}, {mykey:2}, {mykey:3}];
 	collection.insertOne(docs[0]);
 
-	var collection = database.collection('test');
 		collection.find().toArray(function(err, docs) {
 	    console.log(JSON.stringify(docs));
 	});
